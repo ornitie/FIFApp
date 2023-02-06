81b8bca12ba411961de21b68ef3382382adb9c95
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2023-02-05 20:42:31 -0500

    Added QueryParams full fields

--- models/Query.ts
+++ models/Query.ts
@@ -1,8 +1,12 @@
 export interface Query {
   nation?: string;
   playerName?: string;
+  club?: string;
+  league?: string;
 }
 
 export const QueryMapper = {
   nation: "Nation",
+  club: "Club",
+  league: "League",
 };
